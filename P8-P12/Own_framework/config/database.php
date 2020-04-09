b411946def57e7735b99e654a5b8e8ab46d80b23
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2020-04-09 19:37:01 +0700

    check version PHP

--- P8-P12/Own_framework/config/database.php
+++ P8-P12/Own_framework/config/database.php
@@ -1,8 +1,20 @@
 <?php 
 
+/**
+ * Checker Version Of PHP
+ * PHP Varsi 7.4 harus
+ */
+$versi = explode(".", phpversion());
+if((int) $versi[0].$versi[1] < 74) {
+    exit("Tidak mendukung versi phpmu");
+}
+
+/**
+ * Database Konfigurasi
+ */
 trait databaseConfiguration {
     protected $hostname     = "localhost";
     protected $username     = "root";
-    protected $password     = "";
-    protected $database     = "ti41";
+    protected $password     = "kmzwa3awaa";
+    protected $database     = "test";
 }
