619ac4c89986abf925bb5a9530db1e300aabea43
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2020-04-13 11:54:47 +0700

    update config

--- P8-P12/Own_framework/config/database.php
+++ P8-P12/Own_framework/config/database.php
@@ -6,6 +6,6 @@
 trait databaseConfiguration {
     protected $hostname     = "localhost";
     protected $username     = "root";
-    protected $password     = "kmzwa3awaa";
-    protected $database     = "test";
+    protected $password     = "";
+    protected $database     = "ti41";
 }
